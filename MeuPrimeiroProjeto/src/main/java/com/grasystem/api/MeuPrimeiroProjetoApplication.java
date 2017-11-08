6c4696de00389269ff00401a53b4f7d5df6bec8f
Abel Magalhaes - Tecnologia da Informação - OBER S/A <abel@ober.com.br>
2017-11-08 17:58:06 -0200

    Arquivos iniciais do projeto

---
+++ MeuPrimeiroProjeto/src/main/java/com/grasystem/api/MeuPrimeiroProjetoApplication.java
@@ -0,0 +1,12 @@
+package com.grasystem.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuPrimeiroProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
+	}
+}
